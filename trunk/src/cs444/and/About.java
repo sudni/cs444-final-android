fc01698032631d6dd7fcb673443e2160e0fe1b83
liorbk@gmail.com <liorbk@gmail.com@2965ffc2-47d9-d1be-749f-0a14860cdcad>
2011-04-26 03:37:14 +0000

    Added onclick listeners for all the buttons, app icons and about page. Menifest.xml updated to show the relevant changes.

---
+++ trunk/src/cs444/and/About.java
@@ -0,0 +1,13 @@
+package cs444.and;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class About extends Activity{
+	
+	protected void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.about);
+	}
+
+}
