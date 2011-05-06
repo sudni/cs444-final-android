ae2e519d299595607d2fe35de2c79e317e888c29
liorbk <liorbk@gmail.com>
2011-05-06 05:16:37 +0000

    Created timing  ui activity

--- gen/cs444/and/R.java
+++ gen/cs444/and/R.java
@@ -31,6 +31,7 @@ public final class R {
         public static final int about=0x7f030000;
         public static final int future=0x7f030001;
         public static final int main=0x7f030002;
+        public static final int time=0x7f030003;
     }
     public static final class menu {
         public static final int menu=0x7f070000;
@@ -54,6 +55,7 @@ public final class R {
         public static final int settings_label=0x7f060008;
         public static final int settings_shortcut=0x7f06000a;
         public static final int settings_title=0x7f060009;
+        public static final int time_title=0x7f060012;
     }
     public static final class xml {
         public static final int settings=0x7f040000;
